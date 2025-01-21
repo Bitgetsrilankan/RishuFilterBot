59a293bfafe05f7d6ec79db82f1ad1ead31e2b5f
saeedgoraya987 <saeedgoraya987@gmail.com>
2025-01-21 09:09:11 +0500

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2036,7 +2036,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
-  elif query.data == "admin":
+    elif query.data == "admin":
         buttons = [[
             InlineKeyboardButton('⟸ Bᴀᴄᴋ', callback_data='help'),
             InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='help')
