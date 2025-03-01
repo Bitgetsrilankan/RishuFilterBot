0087f6cebc1aced95cb86865065a8ccb84d99606
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-03-01 13:18:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,7 +52,7 @@ async def start(client, message):
                 InlineKeyboardButton('˹ sᴇᴀʀᴄʜ ˼', switch_inline_query_current_chat=''),
                 InlineKeyboardButton('˹ sυᴘᴘσʀᴛ ˼', url=CHNL_LNK)
             ],[
-                InlineKeyboardButton('Hᴇʟᴘ 🕸️', callback_data='help'),
+                InlineKeyboardButton('˹ ʜᴇʟᴘ ˼', callback_data='help'),
                 InlineKeyboardButton('˹ ᴀʙᴏᴜᴛ ˼', callback_data='about')
             ],[
                 InlineKeyboardButton('˹ ᴘʀᴇᴍɪᴜᴍ ᴀɴᴅ ʀᴇғᴇʀʀᴀʟ ˼', callback_data='subscription')
