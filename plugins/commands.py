6bedbe13a9e20d513f55c01bf4a537dbc6c55c83
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-03-01 12:30:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ async def start(client, message):
             InlineKeyboardButton('˹ sυᴘᴘσʀᴛ ˼', url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton('˹ υᴘᴅᴧᴛєs ˼', url=GRP_LNK)
         ],[
-            InlineKeyboardButton('˹ ᴧʟʟ ʙσᴛ's ˼', url=CHNL_LNK)
+            InlineKeyboardButton('˹ ᴧʟʟ ʙσᴛs ˼', url=CHNL_LNK)
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
