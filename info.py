0a17a8575a931bd52259b4e6c597111ea3381bd2
Neoflix <roxr5650@gmail.com>
2025-03-11 12:18:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,16 +57,16 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pegab42884:pegab42884@cluster0.0fgdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "rishuboytboyt")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'venomycollection')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://priyangania010:olmNPdSrLitTh1KO@subsinhalabot.07vko.mongodb.net/?retryWrites=true&w=majority&appName=subsinhalabot")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_NAME = environ.get('DATABASE_NAME', "subsinhalabot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'subcollection')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://levihoy457:levihoy457@cluster0.41x7a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://yofej40824:yofej40824@cluster0.wo4ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://vigigoh687:vigigoh687@cluster0.ozyxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # This Db is for File Data Store When First Db Is Going To Be Full.
+O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
+F_DB_URI = environ.get('F_DB_URI', "")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
