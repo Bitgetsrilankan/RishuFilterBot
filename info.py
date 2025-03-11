3b77978f4af6138c6d5419f0ecd0102f2fdd56bc
Neoflix <roxr5650@gmail.com>
2025-03-11 18:50:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://priyangania010:olmNPdS
 DATABASE_NAME = environ.get('DATABASE_NAME', "subsinhalabot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'subcollection')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://priyangania010:olmNPdSrLitTh1KO@subsinhalabot.07vko.mongodb.net/?retryWrites=true&w=majority&appName=subsinhalabot")   # This Db Is For Other Data Store
