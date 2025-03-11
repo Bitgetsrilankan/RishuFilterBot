e74687c7feb4bcceae0245f463e180bfc1ff3e6f
Neoflix <roxr5650@gmail.com>
2025-03-11 19:42:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,12 +61,12 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://priyangania010:olmNPdS
 DATABASE_NAME = environ.get('DATABASE_NAME', "subsinhalabot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'subcollection')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://priyangania010:olmNPdSrLitTh1KO@subsinhalabot.07vko.mongodb.net/?retryWrites=true&w=majority&appName=subsinhalabot")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://sirila659:GQD9q5KMp2PaBvfd@db1.taovv.mongodb.net/?retryWrites=true&w=majority&appName=db1")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://minideveloper06:nvRrHLViR4j7DALy@db2.q5mql.mongodb.net/?retryWrites=true&w=majority&appName=db2")   # This Db is for File Data Store When First Db Is Going To Be Full.
+O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
+F_DB_URI = environ.get('F_DB_URI', "")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
